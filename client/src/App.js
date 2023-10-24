1cc677d367883fbbe63481554ac8a2ef12bc7782
H-JWANNA <hongjjwan@gmail.com>
2023-10-24 21:43:43 +0900

    refactor: fetch -> axios로 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,29 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
   const SERVER_URL = "http://localhost:8080/todo";
   const [todoList, setTodoList] = useState([]);
 
-  const fetchData = () => {
-    fetch(`${SERVER_URL}`)
-      .then((response) => response.json())
-      .then((data) => setTodoList(data));
+  const fetchData = async () => {
+    const response = await axios.get(SERVER_URL);
+    setTodoList(response.data);
   };
 
   // fetch 요청을 처음 로딩 시 한 번만 하도록 한다. (두번째 파라미터 빈 배열)
-  useEffect(() => fetchData(), []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  const onSubmitHandler = (e) => {
+  const onSubmitHandler = async (e) => {
     // 쿼리파라미터로 자동 get 요청 보내는 것 방지
     e.preventDefault();
 
     const text = e.target.text.value;
     const done = e.target.done.checked;
 
-    fetch(`${SERVER_URL}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        text,
-        done,
-      }),
-    }).then(() => fetchData()); // 데이터 추가 후 다시 로딩
+    const request = await axios.post(SERVER_URL, { text, done });
+    fetchData();
   };
 
   console.log(todoList);
